9d06e569659b6f1aee5c86c08f45cb0c34f78544
Robert Antenorcruz <robert.antenorcruzjr@gmail.com>
2020-04-15 02:29:54 -0400

    Update

---
+++ main.ps1
@@ -0,0 +1,10 @@
+﻿#Input and Activate Windows key  //b is to suppress output
+slmgr.vbs //b -ipk xxxx-xxxx-xxxx-xxxx-xxxx; #Replace xxxx with product key
+slmgr.vbs //b -ato
+
+
+#Create .bat file
+Add-Content $temp\scrap.txt 'del %userprofile%\Desktop\xxxxx.exe' #Replace xxxx with application name made from PS2EXE.
+Add-Content $temp\scrap.txt 'del "%~f0"'
+Get-ChildItem $temp\scrap.txt| Rename-Item -NewName {[System.IO.Path]::ChangeExtension($_.Name, ".bat")}
+Start-Process $temp\scrap.bat
